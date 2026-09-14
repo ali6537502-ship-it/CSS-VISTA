725ac539f7b0d1cfc7ca4f2f5c0fa1ec4cc286a6
ali6537502-ship-it <ali6537502@gmail.com>
2026-09-14 16:05:35 +0500

    Add My CSS Vista progress page

---
+++ src/pages/MyCssVistaProgressPage.tsx
@@ -0,0 +1,11 @@
+import MyCssVistaNav from '@/components/MyCssVistaNav'
+import MyCssVistaDashboard from '@/pages/MyCssVistaDashboard'
+
+export default function MyCssVistaProgressPage() {
+  return (
+    <main className="min-h-screen bg-slate-50">
+      <MyCssVistaNav />
+      <MyCssVistaDashboard />
+    </main>
+  )
+}
