2cc10bc21daaa085ea7c8b13b597689ef9748f9b
ali6537502-ship-it <ali6537502@gmail.com>
2026-09-14 16:05:28 +0500

    Add My CSS Vista tasks page

---
+++ src/pages/MyCssVistaTasksPage.tsx
@@ -0,0 +1,11 @@
+import MyCssVistaNav from '@/components/MyCssVistaNav'
+import StudyPlanner from '@/pages/StudyPlanner'
+
+export default function MyCssVistaTasksPage() {
+  return (
+    <main className="min-h-screen bg-slate-50">
+      <MyCssVistaNav />
+      <StudyPlanner />
+    </main>
+  )
+}
