a9aec679669ef1671e264c5604895e4e7a25d5de
ali6537502-ship-it <ali6537502@gmail.com>
2026-09-21 10:54:10 +0500

    perf: defer task reminder for signed-in users

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
-import { StrictMode } from 'react'
+import { lazy, StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 import { AccountProvider } from './components/AccountProvider.tsx'
 import { SiteContentProvider } from './components/SiteContentProvider.tsx'
-import TaskReminderOverlay from './components/TaskReminderOverlay.tsx'
 import { installChunkRecovery } from './lib/chunkRecovery.ts'
+import { useAccount } from './lib/accountContext.ts'
 
 installChunkRecovery()
+
+const TaskReminderOverlay = lazy(() => import('./components/TaskReminderOverlay.tsx'))
+
+function SignedInTaskReminderOverlay() {
+  const { user } = useAccount()
+  if (!user) return null
+  return <Suspense fallback={null}><SignedInTaskReminderOverlay /></Suspense>
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
