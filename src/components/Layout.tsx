120f57d7b1352bd4ef588d657fea2034b0841b7c
ali6537502-ship-it <ali6537502@gmail.com>
2026-09-22 11:03:21 +0500

    Surface Vistagram in primary desktop and mobile navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ import {
   ClipboardList, Newspaper, Megaphone, Wrench, Gamepad2, UserCheck,
   Landmark, TrendingUp, Languages, Target, LayoutDashboard,
   MessageCircle, ExternalLink, Home as HomeIcon, Globe2, Grid2X2, UserRound,
-  NotebookPen, Video, CalendarRange, FileCheck2, Instagram, Youtube, Flame, BookMarked, BrainCircuit, ShieldCheck, ListTree, LibraryBig, type LucideIcon,
+  NotebookPen, Video, CalendarRange, FileCheck2, Instagram, Youtube, Flame, BookMarked, BrainCircuit, ShieldCheck, ListTree, LibraryBig, Sparkles, type LucideIcon,
 } from 'lucide-react'
 import { STORAGE_FAILED_EVENT } from '@/lib/progressEvents'
 import { site } from '@/data/site'
@@ -121,6 +121,7 @@ const nav = [
 
 const primaryNav = [
   { label: 'Home', to: '/', icon: HomeIcon },
+  { label: 'Vistagram', to: '/vistagram', icon: Sparkles },
   { label: 'Subject MCQs', to: '/css-mcqs', icon: ClipboardList },
   { label: 'GK World', to: '/gk', icon: Globe2 },
   { label: 'MPT Practice', to: '/mpt', icon: PenLine },
@@ -137,6 +138,7 @@ const mobileBottomNav = [
 
 const mobileQuickLinks = [
   { label: 'Home', to: '/', icon: HomeIcon },
+  { label: 'Vistagram', to: '/vistagram', icon: Sparkles },
   ...sortHomeCardsByPriority(defaultHomeCards.filter((item) => item.visible))
     .map((item) => ({
       label: item.title,
@@ -652,6 +654,7 @@ function resolveRouteDirection(locationKey: string, pathname: string, navigation
 }
 
 function fallbackRoute(pathname: string) {
+  if (/^\/vistagram\/[^/]+\/?$/.test(pathname)) return '/vistagram'
   if (/^\/subjects\/compulsory\/[^/]+\/?$/.test(pathname)) return '/subjects/compulsory'
   if (/^\/past-papers\/view\/[^/]+\/?$/.test(pathname)) return '/past-papers'
   if (pathname === '/css-past-paper-analysis') return '/past-papers'
