84c94e74b14a74ad6806e1109c8aa7a832d3e2fa
ali6537502-ship-it <ali6537502@gmail.com>
2026-09-25 12:14:12 +0500

    Set notes bundle offer to PKR 10,000

--- src/data/notesBundleOffer.ts
+++ src/data/notesBundleOffer.ts
@@ -1,5 +1,5 @@
 export const notesBundleOfferEndsAt = Date.parse('2026-09-25T23:00:00+05:00')
-export const notesBundleOfferPrice = 8000
+export const notesBundleOfferPrice = 10000
 
 export function isNotesBundleExclusiveWindow(now = Date.now()) {
   return now < notesBundleOfferEndsAt
